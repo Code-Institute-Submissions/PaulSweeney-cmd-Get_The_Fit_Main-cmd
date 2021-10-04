f15a3e9de72aeaa860129a11cf5131cbef182aa1
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-04 15:55:54 +0000

    Altered Category name in the django admin by crating a meta class to update the spelling

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=300)
     friendly_name = models.CharField(max_length=300, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
