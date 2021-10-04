8de588261881b34b1b32ddfe3e8a40c7f8481319
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-04 15:55:07 +0000

    Created classes for product and category admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import Product, Category
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    field_list_display = (
+        'sku',
+        'name',
+        'description',
+        'detail_01',
+        'detail_02',
+        'detail_03',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    field_list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
