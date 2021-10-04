dbb0421316f837683f1814eab7f0ebc756d3f73a
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-04 16:25:43 +0000

    Re-formatted code due to pasting wrong url patter in at app level, code rectified and saved

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 # adding an empty path to indicate this is the route url
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
 ]
