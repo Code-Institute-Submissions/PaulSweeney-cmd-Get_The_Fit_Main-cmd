3e3fcd529d82eea5da7bab772248f54a8810144b
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-05 18:38:53 +0000

    Created url for individual products view targeting the product id

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.site_products, name='products')
+    path('', views.site_products, name='products'),
+    path('<product_id>/', views.individual_product, name='individual_product'),
 ]
