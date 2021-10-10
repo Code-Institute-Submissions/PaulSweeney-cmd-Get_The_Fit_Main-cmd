4f499516673edbd881adabdac28517222dc09b90
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-10 12:36:43 +0000

    Created a view for the user to add a procust to their shopping bag

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def bag(request):
     """ A view to render the users shopping bag"""
 
     return render(request, 'shopping_bag/shopping_bag.html')
+
+
+def add_item(request, item_id):
+    """ Adding a product quantity to the shopping bag """
+
+    item_quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += item_quantity
+    else:
+        bag[item_id] = item_quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
