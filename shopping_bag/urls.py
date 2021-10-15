665ddb5d9424911c403a5937e25b6dbfa13f820f
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-15 23:19:51 +0000

    Added urls for the update bag and delete item views

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.bag, name='bag'),
     path('add/<item_id>', views.add_item, name='add_item'),
+    path('update/<item_id>', views.update_bag, name='update_bag'),
+    path('delete/<item_id>', views.delete_item, name='delete_item'),
 ]
