1718c68d97b54d3e6aad0f675d5321ebcf225f00
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-17 11:46:51 +0000

    Created view to import checkout form and iterate through session users current bag, display an error if empty

--- store_checkout/views.py
+++ store_checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import NewOrderForm
 
-# Create your views here.
+
+def checkout(request):
+    shopping_bag = request.session.get('bag', {})
+    if not shopping_bag:
+        messages.error(request, 'Sorry, your bag seems to be empty, nothing to display.')
+        return redirect(reverse('products'))
+    # importing the order form created in forms.py,
+    # assigning html to template and creating a context
+    # to contain all details and then rendering it all back.
+    new_order_form = NewOrderForm()
+    template = 'store_checkout/checkout_form.html'
+    context = {
+        'new_order_form': new_order_form,
+    }
+
+    return render(request, template, context)
