f674634d0c2301564bc86d0c363be7aa920dd0b4
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-17 11:02:37 +0000

    Set function with init method to create placeholders for info fields to later remove auto generated field labels

--- store_checkout/forms.py
+++ store_checkout/forms.py
@@ -6,12 +6,13 @@ class NewOrderForm(froms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            'first_name', 'last_name', 'email_address', 
+            'first_name', 'last_name', 'email_address',
             'phone_number', 'address1', 'address2', 'town_or_city',
             'country', 'postcode', 'county',
         )
+
     # removing auto labels, adding placeholders and classes
-    def __init__(*args, **kwags):
+    def __init__(*args, **kwargs):
         placeholders = {
             'first_name': 'First Name',
             'last_name': 'Last Name',
@@ -24,4 +25,5 @@ class NewOrderForm(froms.ModelForm):
             'postcode': 'Post Code',
             'country': 'Country'
         }
-    
+
+        self.fields['first_name', 'last_name'].widget.attrs['autofocus'] = True
