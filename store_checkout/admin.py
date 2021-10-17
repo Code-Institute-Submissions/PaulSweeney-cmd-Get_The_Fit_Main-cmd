d1202985fd41abd5bf0fc44c9c1d4f7a00101532
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-17 09:57:24 +0000

    Added classes with felds and read only fields for diaplying data in django admin

--- store_checkout/admin.py
+++ store_checkout/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('item_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = (
+        'order_number', 'date',
+        'delivery_total', 'bag_total',
+        'grand_total',
+    )
+
+    fields = (
+        'order_number', 'first_name', 'last_name',
+        'email_address', 'phone_number', 'address1',
+        'address2', 'town_or_city', 'county', 'postcode',
+        'country', 'date', 'delivery_total', 'bag_total',
+        'grand_total',
+    )
+
+    list_display = (
+        'order_number', 'date',
+        'first_name', 'last_name',
+        'bag_total', 'delivery_total',
+        'grand_total',
+    )
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
