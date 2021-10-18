55608503243eeede6c2a7f6f8e1f0f06a005ad7d
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-18 20:44:26 +0000

    Format

--- store_checkout/static/checkout_form/stripe/stripe.js
+++ store_checkout/static/checkout_form/stripe/stripe.js
@@ -1,10 +1,10 @@
 // core js to set up functionality for stripe payments
 
 /*---------- Variables ----------*/
-var stripe_publishable_key = $('#id_stripe_publishable_key').text().slice(1, -1);
-var stripe_secret_key = $('#id_stripe_secret_key').text().slice(1, -1);
+var stripePublishableKey = $('#id_stripe_publishable_key').text().slice(1, -1);
+var clientSecret = $('#id_stripe_secret_key').text().slice(1, -1);
 // setting up by creating variables
-var stripe = Stripe(stripe_publishable_key);
+var stripe = Stripe(stripePublishableKey);
 var elements = stripe.elements();
 
 /*---------- Payment styling ----------*/
@@ -25,20 +25,3 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
-
-/*---------- Handling validation errors ----------*/
-
-card.addEventListener('change', function (event) {
-    var cardError = document.getElementById('card-invalid');
-    if (event.error) {
-        var displayError = `
-        <span class="icon" role="alert">
-            <i class="fas fa-exclamation-triangle"></i>
-        </span>
-        <span>${event.error.message}</span>`;
-        $(cardError).html(displayError);
-    } else {
-        cardError.textContent = '';
-    }
-});
-
--- store_checkout/views.py
+++ store_checkout/views.py
@@ -1,49 +1,22 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.conf import settings
-
 from .forms import NewOrderForm
-from shopping_bag.contexts import bag_items
-import stripe
 
 
 def checkout(request):
-    # Extracting keys and setting to variables
-    publishable_key = settings.STRIPE_PUBLISHABLE_KEY
-    secret_key = settings.STRIPE_SECRET_KEY
-
-    # extracting users shopping bag contents/throwing an error if bag is empty
     shopping_bag = request.session.get('bag', {})
     if not shopping_bag:
-        messages.error(request, 'Sorry, your bag seems to be empty, \
-            nothing to display.')
+        messages.error(request, 'Sorry, your bag seems to be empty, nothing to display.')
         return redirect(reverse('products'))
-
-    # getting the current bag contents and grand total from contexts.py
-    current_user_bag = bag_items(request)
-    total = current_user_bag['grand_total']
-    stripe_total = round(total * 100)
-
-    # creating an intent an passing in the currency type and grand total
-    stripe.api_key = secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-
-    # importing the order form created in forms.py
+    # importing the order form created in forms.py,
+    # assigning html to template and creating a context
+    # to contain all details and then rendering it all back.
     new_order_form = NewOrderForm()
-
-    # informing user about missing information and throwing an error
-    if not publishable_key:
-        messages.warning(request, 'Oops, looks like your publishable key is missing! \
-            Check your environment and try again')
-
     template = 'store_checkout/checkout_form.html'
     context = {
         'new_order_form': new_order_form,
-        'publishable_key': publishable_key,
-        'secret_key': intent.secret_key,
+        'stripe_publishable_key': 'pk_test_51JlylWLJjkwVG9it4hZsRIwmBrpde9K1cbjyWQIGxhP8Q9hT203kF8e9RB39YLSre62yUEHXdr6mruia3FJFnk0500No2N6Tms',
+        'stripe_secret_key': 'test stripe secret key',
     }
-    # rendering information to the right template
+
     return render(request, template, context)
