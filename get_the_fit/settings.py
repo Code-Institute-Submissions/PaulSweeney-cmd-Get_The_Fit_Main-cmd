ec0fbfc855f5d3dcaac1998124e76721922b022e
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-18 18:22:02 +0000

    Added secret keys for stripe

--- get_the_fit/settings.py
+++ get_the_fit/settings.py
@@ -23,8 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET KEYS
 SECRET_KEY = os.getenv('SECRET_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
