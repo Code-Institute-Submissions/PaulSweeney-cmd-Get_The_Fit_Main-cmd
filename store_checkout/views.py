203f4bbad8972e67bfe43a726905f28a2fbc2b8c
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-19 17:06:03 +0000

    Added if not statement to throw a warning message in the event of a missing public key

--- store_checkout/views.py
+++ store_checkout/views.py
@@ -8,24 +8,41 @@ from .forms import NewOrderForm
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     shopping_bag = request.session.get('bag', {})
     if not shopping_bag:
         messages.error(request, 'Sorry, your bag seems to be empty, nothing to display.')
         return redirect(reverse('products'))
-    
+    # extracting current items in shopping bag in views.py
     current_bag = bag_items(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
 
-    # importing the order form created in forms.py,
-    # assigning html to template and creating a context
-    # to contain all details and then rendering it all back.
+    # extracting the grand total data from new current_bag variable
+    total = current_bag['grand_total']
+
+    # using round method to round the grand total data to 0 decimal places
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+
+    # creating the payment intent
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    # importing the order form created in forms.py
     new_order_form = NewOrderForm()
+
+    # throw an error if the public key has not ben added to the environment
+    if not stripe_public_key:
+        messages.warning(request, 'Oops, looks like your public key is missing! \
+            Please check your environment and try again')
+
     template = 'store_checkout/checkout_form.html'
     context = {
         'new_order_form': new_order_form,
-        'stripe_publishable_key': 'pk_test_51JlylWLJjkwVG9it4hZsRIwmBrpde9K1cbjyWQIGxhP8Q9hT203kF8e9RB39YLSre62yUEHXdr6mruia3FJFnk0500No2N6Tms',
-        'stripe_secret_key': 'test stripe secret key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
