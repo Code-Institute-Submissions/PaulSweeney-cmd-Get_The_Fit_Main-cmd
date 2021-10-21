87a970cc9dbe55059a6afc47ba0bdfc6547245db
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-21 09:00:46 +0000

    Added required imports

---
+++ store_checkout/webhooks.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.http import HttpResponse
+
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+import stripe
+
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """Listening to Webhooks from stripe"""
+    # Setting up api key and stripe webhook secret
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # Extracting stripe webhook data and verify signature
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct.event(
+            payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
+
+
+    # Handling the event
+    if event.type == 'payment_intent.succeeded':
+        payment_intent = event.data.object  # contains a PaymentIntent
+        print('PaymentIntent was successful')
+
+    elif event.type == 'payment_method.attached':
+        payment_method = event.data.object  # contains a stripe.PaymentMethod
+        print('PaymentMethod was attached to a customer')
+
+    # Handling other event types
+    else:
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=200)
