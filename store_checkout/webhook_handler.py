f604fd095c36a5c0b32a9d978bb3b1d6ec180f1f
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-21 08:17:45 +0000

    Created a class to handle webhooks and an instance of that class to return a successful http respone

---
+++ store_checkout/webhook_handler.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+
+class stripeWH_Handler:
+    """ Handling webhooks from stripe """
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """ Handling webhooks events"""
+
+        return HttpResponse(
+            content=f'Ths webhook has been received successfully \
+                {event["type"]}', status=200
+        )
