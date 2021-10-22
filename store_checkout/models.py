495c94ffc8f13618e465c4a24d00d06e4d4f18c1
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-22 18:20:42 +0000

    Imported CountryField from django, removed models. and replaced with CountryField and migrated changes to django database

--- store_checkout/models.py
+++ store_checkout/models.py
@@ -2,6 +2,8 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
+from django_countries.fields import CountryField
 from products.models import Product
 
 
@@ -16,7 +18,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=30, null=False, blank=False)
     county = models.CharField(max_length=20, null=False, blank=False)
     postcode = models.CharField(max_length=15, null=True, blank=True)
-    country = models.CharField(max_length=50, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateField(auto_now_add=True)
     delivery_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0
