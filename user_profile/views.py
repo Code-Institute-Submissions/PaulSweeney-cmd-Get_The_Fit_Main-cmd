c5770db8de6b6aac32f884657287572009a0ec2e
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-23 18:58:07 +0000

    Created a function to display the users profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def user_profile(request):
+    """ displaying users profile """
+
+    template = 'user_profile/user_profile.html'
+    context = {}
+
+    return render(request, template, context)
