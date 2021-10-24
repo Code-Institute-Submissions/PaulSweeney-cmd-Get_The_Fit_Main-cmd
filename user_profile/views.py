5474cde5422ac15b31d718373d683e339394bdbc
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-24 12:38:41 +0000

    User profile html: passed in templating to render users name to page, updated user_profile view for functionality to render users information to profile page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Profile
 
 
 def user_profile(request):
     """ displaying users profile """
-
+    profile = get_object_or_404(Profile, user=request.user)
     template = 'user_profile/user_profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
