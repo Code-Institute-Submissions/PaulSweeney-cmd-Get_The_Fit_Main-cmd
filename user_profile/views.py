c2dcbd7c182f5a9ca6938f27c0738c38b19c4cab
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-24 19:13:42 +0000

    User_profile.html: updated form|crispy templating to alow user to post infrmation to the database, base.css: added styling for allauth form fields in thje profile page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 from .forms import ProfileForm
 
-
+@login_required
 def user_profile(request):
     """ displaying users profile """
     profile = get_object_or_404(Profile, user=request.user)
+
+    if request.method == 'POST':
+        # generating users information, checking validation
+        # and returning a success message if everything checks out.
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated.')
+
     form = ProfileForm(instance=profile)
     orders = profile.orders.all()
     template = 'user_profile/user_profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
