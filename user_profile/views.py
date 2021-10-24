c7642d7adc4696fb71c6ae60cc721b0e530dadcd
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-24 12:50:18 +0000

    Imported the profile form from forms.py integrated it in to a variable with the profile instance to gather the information from get object or 404 and updated th context to retrun the information

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Profile
+from .forms import ProfileForm
 
 
 def user_profile(request):
     """ displaying users profile """
     profile = get_object_or_404(Profile, user=request.user)
+    form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
     template = 'user_profile/user_profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
