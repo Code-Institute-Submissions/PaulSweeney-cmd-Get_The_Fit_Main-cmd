16c544109ce9926c34b509d96834de45aec7dc2e
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-25 18:34:22 +0000

    Added url for the previous order view

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_profile, name='user_profile')
+    path('', views.user_profile, name='user_profile'),
+    path('previous_orders/<order_number>', views.previous_orders, name='previous_orders'),
 ]
