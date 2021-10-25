3160aa628a2f7092b78936dcc4a272dbfe0f998a
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-25 20:38:22 +0000

    Adde functionality t store users information when checkbox is selected, includes if statetment to determine that if a user is not anonymous this data will save due to anonymous user not having a registered account to ulpdate any profile

--- store_checkout/webhook_handler.py
+++ store_checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderItem
 from products.models import Product
+from user_profile.models import Profile
 
 import json
 import time
@@ -36,6 +37,20 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+        
+        # updating the users profile information when save info box checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnnonymousUser':
+            profile = Profile.objects.get(user__username=username)
+            if save_info:
+                profile.default_country = shipping_details.address.country,
+                profile.default_postcode = shipping_details.address.postcode,
+                profile.default_town_or_city = shipping_details.address.city,
+                profile.default_address1 = shipping_details.address.line1,
+                profile.default_address2 = shipping_details.address.line2,
+                profile.default_county = shipping_details.address.state,
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -48,7 +63,7 @@ class StripeWH_Handler:
                     email_address__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
+                    postcode__iexact=shipping_details.address.postcode,
                     town_or_city__iexact=shipping_details.address.city,
                     address1__iexact=shipping_details.address.line1,
                     address2__iexact=shipping_details.address.line2,
@@ -72,6 +87,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     first_name=shipping_details.name,
+                    user_profile=profile,
                     email_address=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
