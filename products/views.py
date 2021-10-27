38a492a72256217170f482646a150167c827a9d2
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-27 20:13:36 +0000

    Added url for review view to urls.py and also added template for the review form in html

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ReviewForm
 
 
 def site_products(request):
@@ -68,3 +69,15 @@ def individual_product(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_review(request):
+    """ Adding a product review """
+
+    form = ReviewForm()
+    template = 'products/product_detail.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
