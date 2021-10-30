c748600d55177218f07a7570bb6b79f0aed445c2
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-10-30 09:28:54 +0000

    migrated databse again targeting database url from heroku as opposed to original url in settings.py, heroku db url removed and original reinstated

--- get_the_fit/settings.py
+++ get_the_fit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists('env.py'):
@@ -133,8 +134,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
