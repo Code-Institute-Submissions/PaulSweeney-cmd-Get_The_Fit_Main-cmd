6838afc0b453d2f40e04081ec9a1263efb1f8ed3
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-11-28 17:54:12 +0000

    Added url for updating a product

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.site_products, name='products'),
     path('<int:product_id>/', views.individual_product, name='individual_product'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.update_product, name='update_product'),
 ]
