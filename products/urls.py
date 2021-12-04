3610b2e517aa0f2d1fa9f7645c3714f64cb6ac5b
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-12-04 15:49:06 +0000

    Formatted file paths due to length

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.site_products, name='products'),
-    path('<int:product_id>/', views.individual_product, name='individual_product'),
+    path('<int:product_id>/', views.individual_product, name='individual_product\
+        '),
     path('add/', views.add_product, name='add_product'),
-    path('update/<int:product_id>/', views.update_product, name='update_product'),
-    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('update/<int:product_id>/', views.update_product, name='update_product\
+        '),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product\
+        '),
 ]
