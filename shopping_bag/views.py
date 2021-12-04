978c07b4a087adb38f0bf4b17c681af4326a1fa2
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-12-04 00:34:47 +0000

    Rvereted back to original code due to redirect not working

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -88,7 +88,7 @@ def update_bag(request, item_id):
             messages.success(request, f'You have just removed {product.name} from your cart')
 
     request.session['bag'] = current_bag
-    return redirect(reverse('products'))
+    return redirect(reverse('bag'))
 
 
 def delete_item(request, item_id):
@@ -115,8 +115,7 @@ def delete_item(request, item_id):
             messages.success(request, f'You have just removed {product.name} from your cart')
 
         request.session['bag'] = current_bag
-        # return HttpResponse(status=200)
-        return redirect(reverse('products'))
+        return HttpResponse(status=200)
 
     except Exception as e:
         messages.error(request, f'Ooops it looks like there was an error removing: {e}')
